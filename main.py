2384a0478a840634dfdd068021794823e13da5d3
JennyJiang118 <181250057@smail.nju.edu.cn>
2021-01-22 17:30:41 +0800

    revenue

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ def deal_points(data):
 
     # initial starting point
     grid_log.append(data.loc[0,'grid'])
-    points.append(0)
 
     # get grid_log & points
     for i in range(1,len(data)):
@@ -55,17 +54,22 @@ grids = [-18,-15,-12,-9,-6,-3,0,3,6,9,12,15,18]
 data['grid'] = data.gap.apply(lambda x: int(find_grid(x,grids)))
 grid_log, points = deal_points(data)
 
-#---------------------------done here--------------------------#
 
 # %%
+#---------------------------get revenue--------------------------#
 def get_change(grid_log):
     # get change in hold at each deal points
     # 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 3,
-    change_log = grid_log
-    for i in range(len(grid_log),1):
+    change_log = grid_log[:]
+    for i in range(len(grid_log)-1,0,-1):
         change_log[i] = change_log[i]-change_log[i-1]
+    return change_log[1:]
 
 change_log = get_change(grid_log)
+
+def get_revenue(data, change_log, points):
+    for i in range(len(points))：# for each deal
+
 
 
 # def service(data, grid_log, points):
